0f7df7ecab9f330ce5efbc1afd04ff6ab30dbaab
Mirco Veltri <github@mircoveltri.me>
2022-07-15 14:24:48 +0200

    test(Button): to be removed once essentials-next will be released

--- packages/widgets/src/lib/components/buttons/index.ts
+++ packages/widgets/src/lib/components/buttons/index.ts
@@ -1,2 +1,3 @@
+export { default as Button } from './Button.svelte';
 export { default as PrevNextButtons } from './PrevNextButtons.svelte';
 export { default as ScrollToTopButton } from './ScrollToTopButton.svelte';
