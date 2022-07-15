d9058c1848dacaae1849500a7b9eee460c7b111e
Mirco Veltri <github@mircoveltri.me>
2022-07-15 14:26:28 +0200

    chore: exporting new components

--- packages/widgets/src/lib/index.ts
+++ packages/widgets/src/lib/index.ts
@@ -1,13 +1,28 @@
 /** Widgets  */
 import { TOC } from './components/toc';
-import { Card } from './components/card';
+import {
+	Card,
+	CardCover,
+	CardTitle,
+	CardMetadata,
+	CardContent,
+	CardButton
+} from './components/card';
+
+import { ResponsiveCard } from './components/responsive-card';
 import { PrevNextButtons, ScrollToTopButton } from './components/buttons';
 import { CollapsibleList } from './components/list';
 import { Breadcrumbs } from './components/breadcrumbs/';
 import { GitHubButton, GitHubForkRibbon } from './components/github';
 
 export {
+	ResponsiveCard,
 	Card,
+	CardCover,
+	CardTitle,
+	CardMetadata,
+	CardContent,
+	CardButton,
 	TOC,
 	PrevNextButtons,
 	ScrollToTopButton,
