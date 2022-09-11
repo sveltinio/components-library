afd837535c86797ea81ccbf432099f553b2fe24e
Mirco Veltri <github@mircoveltri.me>
2022-09-11 19:31:55 +0200

    fix(esling-config-cutom): no-empty-function rule sets to off

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,7 +2,8 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'turbo', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	rules: {
-		'@typescript-eslint/no-explicit-any': 'off'
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-empty-function': 'off'
 	},
 	settings: {
 		'svelte3/typescript': require('typescript')
