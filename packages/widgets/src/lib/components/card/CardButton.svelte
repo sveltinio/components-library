848d953b8467eeac24b0e36532f397dc96aa430b
Mirco Veltri <github@mircoveltri.me>
2022-11-28 20:35:46 +0100

    refactor(widgets/card): prefetch as boolean

--- packages/widgets/src/lib/components/card/CardButton.svelte
+++ packages/widgets/src/lib/components/card/CardButton.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	export let label = 'Read More »';
 	export let href: string;
-	export let prefetch = '';
+	export let prefetch = false;
 </script>
 
 <div class="card-btn-wrapper" data-testid="card-button-link">
-	<a {href} title={label} data-sveltekit-prefetch={prefetch} aria-label={label} class="card-btn"
-		>{label}</a
+	<a
+		{href}
+		title={label}
+		data-sveltekit-prefetch={prefetch ? '' : 'off'}
+		aria-label={label}
+		class="card-btn">{label}</a
 	>
 </div>
 
