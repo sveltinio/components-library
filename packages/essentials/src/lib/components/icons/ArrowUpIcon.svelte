3f00471c9638f3d7c053d30b9f36bb0266c4afc0
Mirco Veltri <github@mircoveltri.me>
2022-11-29 16:50:50 +0100

    chore(essentials/icons): color set to currentColor

--- packages/essentials/src/lib/components/icons/ArrowUpIcon.svelte
+++ packages/essentials/src/lib/components/icons/ArrowUpIcon.svelte
@@ -4,12 +4,13 @@
 </script>
 
 <svg
+	xmlns="http://www.w3.org/2000/svg"
 	{width}
 	{height}
 	stroke-width="1.5"
 	viewBox="0 0 24 24"
 	fill="none"
-	xmlns="http://www.w3.org/2000/svg"
+	color="currentColor"
 	{...$$restProps}
 >
 	<path
