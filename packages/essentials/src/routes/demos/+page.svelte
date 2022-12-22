c8b7e7f40d461184ecab6d8fc86eab00d69562f3
Mirco Veltri <github@mircoveltri.me>
2022-12-23 00:11:47 +0100

    chore(essentials): demo page updated, styles.css file for button added

--- packages/essentials/src/routes/demos/+page.svelte
+++ packages/essentials/src/routes/demos/+page.svelte
@@ -1,14 +1,58 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+
+	const demos: Array<Record<string, string>> = [
+		{
+			name: 'Button',
+			headline: 'Breadcrumbs helps users to navigate through the website.',
+			url: 'demos/button'
+		},
+		{
+			name: 'ButtonGroup',
+			headline: 'Cards are used to group and display content in a easily readable way.',
+			url: 'demos/button-group'
+		},
+		{
+			name: 'ColorViewer',
+			headline: 'Pages Navigator components to help users to navigate throught the pages.',
+			url: 'demos/color'
+		},
+		{
+			name: 'Dropdown',
+			headline:
+				'ResponsiveCard component display content vertically on small screen, horizontally on large screen.',
+			url: 'demos/dropdown'
+		},
+		{
+			name: 'Link',
+			headline: 'ScrollToTopButton components helps to scroll to the top of the page.',
+			url: 'demos/link'
+		},
+		{
+			name: 'List',
+			headline: 'Table of contents component.',
+			url: 'demos/list'
+		},
+		{
+			name: 'Tabs',
+			headline: 'Table of contents component.',
+			url: 'demos/tabs'
+		}
+	];
+</script>
+
 <section class="main">
-	<h1><tt>@sveltinio/essentials</tt></h1>
-
-	<h2><tt>Demos</tt></h2>
-
-	<a class="link" href="/demos/button"><div class="demo-card">Button</div></a>
-	<a class="link" href="/demos/button-group"><div class="demo-card">ButtonGroup</div></a>
-	<a class="link" href="/demos/color"><div class="demo-card">Color Viewer</div></a>
-	<a class="link" href="/demos/dropdown"><div class="demo-card">Dropdown</div></a>
-	<a class="link" href="/demos/link"><div class="demo-card">Link</div></a>
-	<a class="link" href="/demos/list"><div class="demo-card">List</div></a>
-	<a class="link" href="/demos/scroll-to-top"><div class="demo-card">ScrollToTopButton</div></a>
-	<a class="link" href="/demos/tabs"><div class="demo-card">Tabs</div></a>
+	<h1>@sveltinio/essentials</h1>
+	<h2>Demos</h2>
 </section>
+
+<div class="demo-grid-wrapper">
+	<div class="demo-grid">
+		{#each demos as demo}
+			<a class="demo-card" href="{base}/{demo.url}">
+				<h2>{demo.name}</h2>
+				<p>{demo.headline}</p>
+			</a>
+		{/each}
+	</div>
+</div>
