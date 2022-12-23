bd537be298750ad21cf0177fdbdd3c5281d53117
Mirco Veltri <github@mircoveltri.me>
2022-12-23 16:00:44 +0100

    chore(stylelint-config-custom): selector-class-pattern updated with double dashes for modifiers

--- packages/stylelint-config-custom/index.cjs
+++ packages/stylelint-config-custom/index.cjs
@@ -160,7 +160,7 @@ module.exports = {
 		'selector-attribute-operator-space-before': 'never',
 		'selector-attribute-quotes': 'always',
 		'selector-class-pattern': [
-			'^([a-z][a-z0-9]*)(__[a-z0-9]+)?(__)?([a-z0-9]+)?(-[a-z0-9]+)*$',
+			'^([a-z][a-z0-9]*)(__[a-z0-9]+)?(__)?([a-z0-9]+)?(-+[a-z0-9]+)*$',
 			{}
 		],
 		'selector-combinator-space-after': 'always',
