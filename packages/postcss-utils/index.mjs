d4f238b5b5f28cc5a80d613ba9f1c4f3df09acad
Mirco Veltri <github@mircoveltri.me>
2023-01-11 23:40:23 +0100

    chore(postcss-utils): deps updated, findByName added and findByExtension accepts string|string[]

--- packages/postcss-utils/index.mjs
+++ packages/postcss-utils/index.mjs
@@ -7,18 +7,54 @@ import postcssPresetEnv from 'postcss-preset-env';
 import cssnanoPlugin from 'cssnano';
 import stylelint from 'stylelint';
 
-export async function findByExtension(where, targetExt) {
+/**
+ * Returns the files list at specified path with specific extensions
+ * @param {string} where - The path where to look for files.
+ * @param {string|string[]} ext - The file extension or an array of file extensions.
+ * @returns {string[]} - The files list.
+ */
+export async function findByExtension(where, ext) {
 	let results = [];
 	let files = await fsp.readdir(where, { withFileTypes: true });
 
 	for (let file of files) {
 		let fullPath = path.join(where, file.name);
 		if (file.isDirectory()) {
-			results = [...results, ...(await findByExtension(fullPath, targetExt))];
+			results = [...results, ...(await findByExtension(fullPath, ext))];
 		} else if (file.isFile()) {
 			// file extension without leading "."
-			const ext = path.extname(file.name).slice(1);
-			if (ext === targetExt) {
+			const extension = path.extname(file.name).slice(1);
+
+			if (Array.isArray(matchExts)) {
+				matchExts.forEach((x) => {
+					if (extension === x) results.push(fullPath);
+				});
+			} else if (typeof matchExts == 'string') {
+				if (extension === ext) results.push(fullPath);
+			} else {
+				console.error('Invalid type. Must be a string or an array of strings');
+			}
+		}
+	}
+	return results;
+}
+
+/**
+ * Returns the files list at specified path with specific extensions
+ * @param {string} where - The path where to look for files.
+ * @param {string} name - The file name.
+ * @returns {string[]} - The files list.
+ */
+export async function findByName(where, name) {
+	let results = [];
+	let files = await fsp.readdir(where, { withFileTypes: true });
+
+	for (let file of files) {
+		let fullPath = path.join(where, file.name);
+		if (file.isDirectory()) {
+			results = [...results, ...(await findByName(fullPath, name))];
+		} else if (file.isFile()) {
+			if (file.name === name) {
 				results.push(fullPath);
 			}
 		}
@@ -26,12 +62,20 @@ export async function findByExtension(where, targetExt) {
 	return results;
 }
 
+/**
+ *
+ * @param {string} input - the input file to process
+ * @param {string} output - the filepath for the processed file
+ * @param {string} mode - process.env.NODE_ENV
+ * @returns {void}
+ */
 export function postcssProcess(input, output, mode) {
 	const dev = mode === 'development';
 
 	let plugins = [
 		postcssPresetEnv({
 			features: {
+				'custom-media-queries': true,
 				'nesting-rules': true
 			}
 		})
@@ -45,7 +89,7 @@ export function postcssProcess(input, output, mode) {
 				cache: false
 			}),
 			cssnanoPlugin({
-				preset: 'default'
+				preset: 'advanced'
 			})
 		];
 	}
