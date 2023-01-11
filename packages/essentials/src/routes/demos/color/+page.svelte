e39ff185f2829ab3bd115f8024f77b878c894c12
Mirco Veltri <github@mircoveltri.me>
2023-01-12 00:35:21 +0100

    fix(essentials/color): label color

--- packages/essentials/src/routes/demos/color/+page.svelte
+++ packages/essentials/src/routes/demos/color/+page.svelte
@@ -4,18 +4,45 @@
 	const myStyles = {
 		'border-color': '#075985',
 		'border-width': '2px',
-		color: '#0284c7'
+		color: '#075985'
 	};
 </script>
 
-<section class="main">
-	<h1><tt>@sveltinio/essentials</tt></h1>
-	<h2><tt>ColorViewer</tt></h2>
-</section>
+<div class="header" role="main">
+	<h1>@sveltinio/essentials</h1>
+	<h2>ColorViewer</h2>
+</div>
 
-<section>
-	<ColorViewer value="#86198f" />
-	<ColorViewer value="#22d3ee" border showValue={false} />
+<section role="contentinfo">
+	<div class="actions">
+		<a class="btn-back" href="/demos">← Return To The Demos</a>
+	</div>
 
-	<ColorViewer value="#0ea5e9" border styles={myStyles} />
+	<div id="colorviewer-default-wrapper" class="demo-wrapper">
+		<h3 id="colorviewer-default">Default</h3>
+		<div class="preview">
+			<ColorViewer value="#86198f" />
+		</div>
+	</div>
+
+	<div id="colorviewer-label-color-wrapper" class="demo-wrapper">
+		<h3 id="colorviewer-label-color-prop">Label color</h3>
+		<div class="preview">
+			<ColorViewer value="#0ea5e9" labelColor="#075985" border />
+		</div>
+	</div>
+
+	<div id="colorviewer-no-value-wrapper" class="demo-wrapper">
+		<h3 id="colorviewer-no-value">No Label Value</h3>
+		<div class="preview">
+			<ColorViewer value="#22d3ee" border showLabel={false} />
+		</div>
+	</div>
+
+	<div id="colorviewer-theming-with-prop-wrapper" class="demo-wrapper">
+		<h3 id="colorviewer-theming-with-prop">Custom Styles with prop</h3>
+		<div class="preview">
+			<ColorViewer value="#0ea5e9" labelColor="#075985" border styles={myStyles} />
+		</div>
+	</div>
 </section>
