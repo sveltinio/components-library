842702162e17616f35c772e48b8300faaf286913
Mirco Veltri <github@mircoveltri.me>
2023-01-12 10:28:08 +0100

    chore(essentials): simplifying css-minify.js script and make use of findByExtension

--- packages/essentials/scripts/css-minifier.js
+++ packages/essentials/scripts/css-minifier.js
@@ -1,36 +1,19 @@
 import path from 'path';
-import fs from 'fs';
 import pc from 'picocolors';
-import { findByName, postcssProcess } from 'postcss-utils';
+import { findByExtension, postcssProcess } from 'postcss-utils';
 
 function main() {
 	const mode = process.env.NODE_ENV;
 	const distFolder = path.join('dist', 'styles');
 
 	console.log(pc.blue(pc.bold('-> Minify CSS files\n')));
-	findByName(distFolder, 'variables.css')
-		.then((filesList) => {
-			filesList.forEach((input) => {
-				const parsedInput = path.parse(input);
-				const output = path
-					.join(parsedInput.dir, parsedInput.name)
-					.concat('.min')
-					.concat(parsedInput.ext);
-				postcssProcess(input, output, mode);
-				fs.rmSync(input);
-			});
-		})
-		.catch((err) => {
-			console.log(pc.red(pc.bold(err)));
-		});
-
-	findByName(distFolder, ['colors.css', 'tw-preflight.css'])
+	findByExtension(distFolder, ['css'])
 		.then((filesList) => {
 			filesList.forEach((input) => {
 				const parsedInput = path.parse(input);
 				const output = path.join(parsedInput.dir, parsedInput.name).concat(parsedInput.ext);
+				// minify
 				postcssProcess(input, output, mode);
-				fs.rmSync(input);
 			});
 		})
 		.catch((err) => {
