e9bf421b012f174d191ec0083783babb4e8ddfe7
Mirco Veltri <github@mircoveltri.me>
2023-04-13 11:36:52 +0200

    fix(widgets): line clamp on Card and ResponsiveCard

--- packages/widgets/src/lib/components/card/CardContent.svelte
+++ packages/widgets/src/lib/components/card/CardContent.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	export let content: string;
-	export let lineClamp = 2;
+	export let lineClamp: number;
 </script>
 
 <div class="card__content" data-testid="card_content">
-	<p style:-webkit-line-clamp={lineClamp} data-testid="card_content_text">
+	<p class="line-clamp" style:-webkit-line-clamp={lineClamp} data-testid="card_content_text">
 		{content}
 	</p>
 </div>
+
+<style>
+	.line-clamp {
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+	}
+</style>
--- packages/widgets/src/lib/components/responsive-card/CardContent.svelte
+++ packages/widgets/src/lib/components/responsive-card/CardContent.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	export let content: string;
-	export let lineClamp = 4;
+	export let lineClamp: number;
 </script>
 
 <div class="card__content">
-	<p style:-webkit-line-clamp={lineClamp}>
+	<p class="line-clamp" style:-webkit-line-clamp={lineClamp}>
 		{content}
 	</p>
 </div>
+
+<style>
+	.line-clamp {
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+	}
+</style>
