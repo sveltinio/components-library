1beeaa06163cb8ae44535300a45f85b38edbc7e8
Mirco Veltri <github@mircoveltri.me>
2023-04-14 08:51:53 +0200

    refactor(widgets): line clamp for Card component

--- packages/widgets/src/lib/components/card/CardContent.svelte
+++ packages/widgets/src/lib/components/card/CardContent.svelte
@@ -4,15 +4,7 @@
 </script>
 
 <div class="card__content" data-testid="card_content">
-	<p class="line-clamp" style:-webkit-line-clamp={lineClamp} data-testid="card_content_text">
+	<p style:-webkit-line-clamp={lineClamp} data-testid="card_content_text">
 		{content}
 	</p>
 </div>
-
-<style>
-	.line-clamp {
-		display: -webkit-box;
-		-webkit-box-orient: vertical;
-		overflow: hidden;
-	}
-</style>
