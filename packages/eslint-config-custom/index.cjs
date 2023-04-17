30076a99fb8ea894417ad8efa1820d58ef024067
Mirco Veltri <github@mircoveltri.me>
2023-04-17 17:26:31 +0200

    chore(packages/eslint-config-custom): setup prettier-plugin-svelte

--- packages/eslint-config-custom/index.cjs
+++ packages/eslint-config-custom/index.cjs
@@ -1,19 +1,34 @@
 module.exports = {
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'turbo', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'turbo',
+		'prettier'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	plugins: ['@typescript-eslint'],
 	rules: {
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-empty-function': 'off'
 	},
+	ignorePatterns: ['*.cjs', 'packages/seo/src/lib/types.ts'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	settings: {
 		'svelte3/typescript': require('typescript')
-	},
-	ignorePatterns: ['*.cjs', 'packages/seo/src/lib/types.ts'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
 	},
 	env: {
 		browser: true,
