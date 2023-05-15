08617fdb161e79c74e68dd6070f45d1805e7d2e1
Mirco Veltri <github@mircoveltri.me>
2023-05-15 12:01:40 +0200

    refactor(essentials): retrieveCssClassNames in src/lib/utils.ts

--- packages/essentials/src/lib/utils.ts
+++ packages/essentials/src/lib/utils.ts
@@ -1,12 +1,24 @@
 import { contains } from '@sveltinio/ts-utils/collections';
 import { isNullish } from '@sveltinio/ts-utils/is';
 
+/**
+ * Retrieves the CSS class names from a value, excluding any reserved names.
+ * If the value is nullish, an empty string is returned.
+ *
+ * @param value - The value to retrieve CSS class names from.
+ * @param reservedNames - An array of reserved class names to exclude.
+ * @returns The retrieved CSS class names or an empty string if reserved names are found.
+ */
 export function retrieveCssClassNames(value: any, reservedNames: string[]): string {
-	if (isNullish(value)) return '';
-	const classesArray = String(value).split(' ');
+	if (isNullish(value)) {
+		return '';
+	}
 
-	let result = '';
-	classesArray.some((v) => contains(reservedNames, v)) ? (result = '') : (result = value);
+	const cssClasses = String(value).split(' ');
 
-	return result;
+	if (cssClasses.some((cssClass) => contains(reservedNames, cssClass))) {
+		return '';
+	}
+
+	return value;
 }
