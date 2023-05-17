cde58817d48f3e236bb7061f585eeefc7a54a08b
Mirco Veltri <github@mircoveltri.me>
2023-05-17 11:58:30 +0200

    fix(widgets): commit missing actions.ts file

---
+++ packages/widgets/src/lib/actions.ts
@@ -0,0 +1,63 @@
+import type { Action } from 'svelte/action';
+
+export interface ClickOutsideActionOptions {
+	enabled: boolean;
+	cb?: (node: HTMLElement) => void;
+}
+
+export const clickOutsideAction: Action<HTMLElement, ClickOutsideActionOptions> = (
+	node,
+	options
+) => {
+	function eventHandler(event: MouseEvent) {
+		const target = event.target as Node;
+		if (node && !node.contains(target) && !event.defaultPrevented) {
+			if (options?.cb) options.cb(node);
+		}
+	}
+
+	if (options?.enabled) window.addEventListener('click', eventHandler);
+
+	return {
+		update(param) {
+			if (param.enabled) window.addEventListener('click', eventHandler);
+		},
+		destroy() {
+			window.removeEventListener('click', eventHandler);
+		}
+	};
+};
+
+export interface ActiveActionOptions {
+	enabled: boolean;
+	className: string;
+}
+
+export const activeAction: Action<HTMLElement, ActiveActionOptions> = (
+	node,
+	options = { enabled: true, className: 'is-active' }
+) => {
+	addActiveClass(node, options.className);
+
+	return {
+		update: (params) => {
+			if (params.enabled) addActiveClass(node, params.className);
+		},
+		destroy: () => node.classList.remove(options.className)
+	};
+};
+
+function isActive(href: string): boolean {
+	const { hash, pathname, search } = new URL(location.href);
+	const path = pathname + search + hash;
+	return path.includes(href);
+}
+
+function addActiveClass(node: HTMLElement, className: string): void {
+	const parentElement = node.parentElement;
+	if (isActive(node.getAttribute('href') || '')) {
+		parentElement?.classList.add(className);
+	} else {
+		parentElement?.classList.remove(className);
+	}
+}
--- packages/widgets/src/lib/components/toc/TOC.svelte
+++ packages/widgets/src/lib/components/toc/TOC.svelte
@@ -2,14 +2,13 @@
 	import '../../styles/base.css';
 	import '../../styles/components/toc/variables.css';
 	import '../../styles/components/toc/styles.css';
-	import { onMount, tick } from 'svelte';
 	import type { TocContext, TocEntry } from './types.js';
 	import { retrieveCssClassNames } from '$lib/utils';
 	import { mapToCssVars } from '@sveltinio/ts-utils/objects';
 	import TocList from './TocList.svelte';
 	import TocButton from './TocButton.svelte';
-	import { clickOutsideAction } from '$lib/actions';
-	import { a11yKeyboardAction } from './a11y-keyboard';
+	import { clickOutsideAction } from '$lib/actions.js';
+	import { a11yKeyboardAction } from './a11y-keyboard.js';
 	import { writable } from 'svelte/store';
 	import { setContext } from 'svelte';
 
