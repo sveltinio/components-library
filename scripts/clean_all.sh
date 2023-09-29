954ed21725abf38be25d1f14a0e4b642f8230e97
indaco <github@mircoveltri.me>
2023-09-29 11:11:57 +0200

    refactor(scripts): remove turbo and use pnpm

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -4,7 +4,7 @@ projectFolder=".."
 echo "- Cleaning the project root folder"
 rm -rf "${projectFolder}/node_modules"
 
-foldersToRemove=("dist" ".svelte-kit" ".turbo" "node_modules" "coverage")
+foldersToRemove=("dist" ".svelte-kit" "node_modules" "coverage")
 
 packagesFolder="${projectFolder}/packages"
 packagesList=("eslint-config-custom" "stylelint-config-custom" "postcss-utils" "essentials" "media-content" "seo" "services" "widgets")
