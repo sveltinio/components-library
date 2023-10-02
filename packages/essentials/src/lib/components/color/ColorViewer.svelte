adf937c1758d28a4736ef54d60bddff982a4cd4c
indaco <github@mircoveltri.me>
2023-10-02 18:45:36 +0200

    style(essentials): update ColorViewer using open-props

--- packages/essentials/src/lib/components/color/ColorViewer.svelte
+++ packages/essentials/src/lib/components/color/ColorViewer.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import '../../styles/base.css';
-	import '../../styles/components/color/variables.css';
+	import '../../styles/baseline.css';
 	import '../../styles/components/color/styles.css';
 	import { mapToCssVars } from '@sveltinio/ts-utils/objects';
-	import { retrieveCssClassNames } from '$lib/utils';
+	import { retrieveCssClassNames } from '$lib/utils.js';
 
 	export let value: string;
 	export let size = 24;
@@ -18,14 +17,14 @@
 	}
 
 	// avoid hacking reserved css class names
-	const reservedNames = ['sn-e-colors', 'sn-e-c-colorviewer-vars', 'sn-e-c-colorviewer'];
+	const reservedNames = ['sn-e-c-colorviewer'];
 	const cssClasses = retrieveCssClassNames($$props.class, reservedNames);
 </script>
 
 <div
-	class="sn-e-colors sn-e-c-colorviewer-vars sn-e-c-colorviewer {cssClasses}"
+	class="sn-e-c-colorviewer {cssClasses}"
 	style={cssStyles.value}
-	style:color={labelColor != '' ? labelColor : 'currentColor'}
+	style:color={labelColor != '' ? labelColor : 'var(--gray-7)'}
 >
 	<div
 		class="color--preview"
